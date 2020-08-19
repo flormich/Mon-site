e07b6ef1fbc0b746eb182c14d7716420949e3c52
flormich <michelflorian@yahoo.fr>
2020-08-20 01:04:36 +0200

    Add diaporama and vectorisation du code

--- js/script.js
+++ js/script.js
@@ -10,34 +10,45 @@ function afficheMiniature(type, sujet){
 				j=0;
 				i--;
 			} else {
-			var image = ('<a href="../images/' + type + '/' + sujet + ' (' + i + ').jpg"><img src="../images/Miniatures/' + type + '/' + sujet + ' (' + i + ').jpg" border="2px solid white" ></a></img>&ensp;');
+			var image = ('<a href="../images/' + type + '/' + sujet + ' (' + i + ').jpg"><img src="../images/Miniatures/' + type + '/' + sujet + ' (' + i + ').jpg" border="2px solid white" ></a></img>');
 			var newSujet = sujet.ucFirst();
 			var deb = ('<h1><a href="../index.html#' + type + '">' + newSujet + '</a></h1>')
 			finalImage = finalImage + image;
 			j++;
 			} 
 		}
-	var photoMiniature = deb + finalImage;
-	document.getElementById('div2').innerHTML += finalImage;	
+	document.getElementById('div2').innerHTML += finalImage;
 }
 
-function afficheInfo(sujet){
+function afficheInfo(type, sujet){
 	//Fait un UpperCase
 	String.prototype.ucFirst=function(){return this.substr(0,1).toUpperCase()+this.substr(1);}
 	var newSujet = sujet.ucFirst();
-	var contains = ('<h2>' + newSujet +'</h2><ol><li>Distance du soleil : </li><li>Dimètre apparent : exemple</li><li>Distance de la terre : </li><li>Tps que met la lumière à nous atteindre : </li><li>Tps des vidéos : 2 minutes</li><li>Tps de rotation : </li></ol>');
+	if (type == "Astro"){
+		var contains = ('<h2>' + newSujet +'</h2><ol><li>Distance du soleil : </li><li>Diamètre apparent : exemple</li><li>Distance de la terre : </li><li>Tps que met la lumière à nous atteindre : </li><li>Tps des vidéos : 2 minutes</li><li>Tps de rotation : </li></ol>');
+	} if (type == "Paysage"){
+		var contains = ('<h2>' + newSujet +'</h2><ol><li>Lieu : </li><li>Date : </li></ol>');
+	} if (type == "Portrait"){
+		var contains = ('<h2>' + newSujet +'</h2><ol><li>Lieux : </li><li>Date : </li></ol>');
+	}
 	document.getElementById('aside1').innerHTML += contains;
 }
 
-function menuHorizontal(sujet){
-	console.log ("Hello world");
-	var contains = ('<h2>Objets Photographiés</h2><section><ul><li class="titremenu1"><a href="viewPicture.html?Astro&soleil" target="">Soleil</a></li><li class="titremenu2"><a href="viewPicture.html?Astro&mercure" target="">Mercure</a></li><li class="titremenu3"><a href="viewPicture.html?Astro&venus" target="">Vénus</a></li><li class="titremenu4"><a href="viewPicture.html?Astro&terre" target="">Terre</a></li><li class="titremenu5"><a href="viewPicture.html?Astro&lune" target="">Lune</a></li><li class="titremenu6"><a href="viewPicture.html?Astro&mars" target="">Mars</a></li><li class="titremenu7"><a href="viewPicture.html?Astro&jupiter" target="">Jupiter</a></li><li class="titremenu8"><a href="viewPicture.html?Astro&saturne" target="">Saturne</a></li><li class="titremenu9"><a href="viewPicture.html?Astro&ciel_profond" target="">Objets lointains</a></li><li class="titremenu10"><a href="viewPicture.html?Astro&comete" target="">Comète</a></li></ul></section>');
+function menuHorizontal(type, sujet){
+	if (type == "Astro"){		
+		var contains = ('<a href="../index.html"><h2>Astrophotographie</h2></a><section><ul><li class="titremenu1"><a href="viewPicture.html?Astro&soleil" target="">Soleil</a></li><li class="titremenu2"><a href="viewPicture.html?Astro&mercure" target="">Mercure</a></li><li class="titremenu3"><a href="viewPicture.html?Astro&venus" target="">Vénus</a></li><li class="titremenu4"><a href="viewPicture.html?Astro&terre" target="">Terre</a></li><li class="titremenu5"><a href="viewPicture.html?Astro&lune" target="">Lune</a></li><li class="titremenu6"><a href="viewPicture.html?Astro&mars" target="">Mars</a></li><li class="titremenu7"><a href="viewPicture.html?Astro&jupiter" target="">Jupiter</a></li><li class="titremenu8"><a href="viewPicture.html?Astro&saturne" target="">Saturne</a></li><li class="titremenu9"><a href="viewPicture.html?Astro&ciel_profond" target="">Objets lointains</a></li><li class="titremenu10"><a href="viewPicture.html?Astro&comete" target="">Comète</a></li></ul></section>');
+	} if (type == "Paysage"){
+		var contains = ('<a href="../index.html"><h2>Paysages</h2></a><section><ul><li class="titremenu1"><a href="viewPicture.html?Paysage&rhone" target="">Dans le Rhone</a></li><li class="titremenu2"><a href="viewPicture.html?Paysage&isere" target="">En Isère</a></li><li class="titremenu3"><a href="viewPicture.html?Paysage&drome" target="">En drôme</a></li><li class="titremenu4"><a href="viewPicture.html?Paysage&savoie" target="">En savoie</a></li><li class="titremenu5"><a href="viewPicture.html?Paysage&savoieHaute" target="">En haute savoie</a></li><li class="titremenu6"><a href="viewPicture.html?Paysage&ardeche" target="">En ardèche</a></li><li class="titremenu7"><a href="viewPicture.html?Paysage&france" target="">autre département</a></li><li class="titremenu8"><a href="viewPicture.html?Paysage&etranger" target="">Hors de France</a></li></ul></section>');
+	} if (type == "Portrait"){
+		var contains = ('<a href="../index.html"><h2>Portrait</h2></a><section><ul><li class="titremenu1"><a href="viewPicture.html?Portrait&anais" target="">Anaïs</a></li><li class="titremenu2"><a href="viewPicture.html?Portrait&mathilde" target="">Mathilde</a></li><li class="titremenu3"><a href="viewPicture.html?Portrait&johan" target="">Johan</a></li></ul></section>');
+	}
 	document.getElementById('div1').innerHTML += contains;
 }
 
-
-
-
+function diaporama(type, sujet){
+	var contains = ('<a href="diaporama.html?' + type + '&' + sujet +'" target="_blank"><img src="../images/Icon/diaporama2.jpg" title="Lire le diaporama"></a></img>&emsp;');
+	document.getElementById('diap').innerHTML += contains;
+}
 
 
 //Test de fonction
