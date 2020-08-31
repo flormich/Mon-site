1dd0804f9519cbec8473f0cf6290e74b1e8bb07d
flormich <michelflorian@yahoo.fr>
2020-09-01 00:38:45 +0200

    Affichage en fondu enchainé

--- js/script.js
+++ js/script.js
@@ -3,17 +3,38 @@ var beaucoupNbrImage = 41;
 
 //Tableau
 var detailAstronomie = {
-	soleil:{distSol:"0",diam:"",rotation:"24h",revolution:"",sat:"",distTerre:"",diamAppMaxi:"32\'27\"",magnMax:"-26,73",tpsLumTerre:"8 minutes",particularite:"Dégage lumière et chaleur"},
-	mercure:{distSol:"57",diam:"4 879 km",rotation:"59 jours",revolution:"88 jours",sat:"0",distTerre:"",diamAppMaxi:"13\"",magnMax:"-2",tpsLumTerre:"",particularite:"Planéte grillée"},
-	venus:{distSol:"108",diam:"12 100 km",rotation:"244 jours",revolution:"225 jours",sat:"0",distTerre:"108 millions km",diamAppMaxi:"58\"",magnMax:"-4,4",tpsLumTerre:"",particularite:"Rotation en sens inverse"},
-	terre:{distSol:"149",diam:"12 750 km",rotation:"24h",revolution:"365 jours",sat:"1",distTerre:"",diamAppMaxi:"",magnMax:"",tpsLumTerre:"-",particularite:"Contiens de la vie"},
+	soleil:{distSol:"0",diam:"1,39 millions km",rotation:"28 jours",revolution:"-",sat:"",distTerre:"",diamAppMaxi:"32\'27\"",magnMax:"-26,73",tpsLumTerre:"8 minutes",particularite:"Dégage lumière et chaleur"},
+	mercure:{distSol:"57 millions km",diam:"4 879 km",rotation:"59 jours",revolution:"88 jours",sat:"0",distTerre:"",diamAppMaxi:"13\"",magnMax:"-2",tpsLumTerre:"",particularite:"Planéte grillée"},
+	venus:{distSol:"108 millions km",diam:"12 100 km",rotation:"244 jours",revolution:"225 jours",sat:"0",distTerre:"108 millions km",diamAppMaxi:"58\"",magnMax:"-4,4",tpsLumTerre:"",particularite:"Rotation en sens inverse"},
+	terre:{distSol:"149 millions km",diam:"12 750 km",rotation:"24h",revolution:"365 jours",sat:"1",distTerre:"",diamAppMaxi:"",magnMax:"",tpsLumTerre:"-",particularite:"Contiens de la vie"},
 	lune:{distSol:"",diam:"1 474 km",rotation:"29,5 jours",revolution:"29,5 jours",sat:"-",distTerre:"300 000 km",diamAppMaxi:"31\'36\"",magnMax:"-12",tpsLumTerre:"1 sec",particularite:"Sans atmosphere"},
-	mars:{distSol:"228",diam:"6 785 km",rotation:"24h 37min",revolution:"687 jours",sat:"2",distTerre:"",diamAppMaxi:"25\"",magnMax:"-2,8",tpsLumTerre:"4 minutes",particularite:"Plus de champs magnetique"},
-	jupiter:{distSol:"777",diam:"139 820 km",rotation:"9h 53min",revolution:"1ans 11mois",sat:"30",distTerre:"",diamAppMaxi:"50\"",magnMax:"-3",tpsLumTerre:"35 minutes",particularite:"Grande tâche rouge"},
-	saturne:{distSol:"1 428",diam:"1116 460 km",rotation:"10h 25min",revolution:"29ans 6mois",sat:"40",distTerre:"",diamAppMaxi:"20\"",magnMax:"-0,5",tpsLumTerre:"71 minutes",particularite:"Anneaux"},
-	uranus:{distSol:"2 871",diam:"50 725 km",rotation:"17h 15min",revolution:"84ans",sat:"16",distTerre:"",diamAppMaxi:"4\"",magnMax:"5,9",tpsLumTerre:"2 heures 40",particularite:"Couché sur son orbite"},
-	neptune:{distSol:"4 500",diam:"49 244 km",rotation:"18h 30min",revolution:"164ans",sat:"8",distTerre:"",diamAppMaxi:"2,5\"",magnMax:"7,7",tpsLumTerre:"4 heures",particularite:"Grande tâche bleu"},
-	pluton:{distSol:"5 912",diam:"2 375 km",rotation:"6 jours 9h",revolution:"248ans",sat:"1",distTerre:"",diamAppMaxi:"0,1\"",magnMax:"13,7",tpsLumTerre:"",particularite:""},
+	mars:{distSol:"228 millions km",diam:"6 785 km",rotation:"24h 37min",revolution:"687 jours",sat:"2",distTerre:"",diamAppMaxi:"25\"",magnMax:"-2,8",tpsLumTerre:"4 minutes",particularite:"Plus de champs magnetique"},
+	jupiter:{distSol:"777 millions km",diam:"139 820 km",rotation:"9h 53min",revolution:"1ans 11mois",sat:"30",distTerre:"",diamAppMaxi:"50\"",magnMax:"-3",tpsLumTerre:"35 minutes",particularite:"Grande tâche rouge"},
+	saturne:{distSol:"1 428 millions km",diam:"1116 460 km",rotation:"10h 25min",revolution:"29ans 6mois",sat:"40",distTerre:"",diamAppMaxi:"20\"",magnMax:"-0,5",tpsLumTerre:"71 minutes",particularite:"Anneaux"},
+	uranus:{distSol:"2 871 millions km",diam:"50 725 km",rotation:"17h 15min",revolution:"84ans",sat:"16",distTerre:"",diamAppMaxi:"4\"",magnMax:"5,9",tpsLumTerre:"2 heures 40",particularite:"Couché sur son orbite"},
+	neptune:{distSol:"4 500 millions km",diam:"49 244 km",rotation:"18h 30min",revolution:"164ans",sat:"8",distTerre:"",diamAppMaxi:"2,5\"",magnMax:"7,7",tpsLumTerre:"4 heures",particularite:"Grande tâche bleu"},
+	pluton:{distSol:"5 912 millions km",diam:"2 375 km",rotation:"6 jours 9h",revolution:"248ans",sat:"1",distTerre:"",diamAppMaxi:"0,1\"",magnMax:"13,7",tpsLumTerre:"",particularite:""},
+	voie_lactee:{distSol:"Aucune info",diam:"Aucune info",rotation:"Aucune info",revolution:"Aucune info",sat:"Aucune info",distTerre:"Aucune info",diamAppMaxi:"Aucune info",magnMax:"Aucune info",tpsLumTerre:"Aucune info",particularite:"Aucune info"},
+	circumPolaire:{distSol:"Aucune info",diam:"Aucune infom",rotation:"Aucune info",revolution:"Aucune info",sat:"Aucune info",distTerre:"Aucune info",diamAppMaxi:"Aucune info",magnMax:"Aucune info",tpsLumTerre:"Aucune info",particularite:"Aucune info"},
+	ciel_profond:{distSol:"Aucune info",diam:"Aucune info",rotation:"Aucune info",revolution:"Aucune info",sat:"Aucune info",distTerre:"Aucune info",diamAppMaxi:"Aucune info",magnMax:"Aucune info",tpsLumTerre:"Aucune info",particularite:"Aucune info"},
+	comete:{distSol:"Aucune info",diam:"Aucune info",rotation:"Aucune info",revolution:"Aucune info",sat:"Aucune info",distTerre:"Aucune info",diamAppMaxi:"Aucune info",magnMax:"Aucune info",tpsLumTerre:"Aucune info",particularite:"Aucune info"}
+};
+var detailPortrait = {
+	anais:{distSol:"0",diam:"1,39 millions km",rotation:"28 jours",revolution:"-",sat:"",distTerre:"",diamAppMaxi:"32\'27\"",magnMax:"-26,73",tpsLumTerre:"8 minutes",particularite:"Dégage lumière et chaleur"},
+	anais2:{distSol:"57",diam:"4 879 km",rotation:"59 jours",revolution:"88 jours",sat:"0",distTerre:"",diamAppMaxi:"13\"",magnMax:"-2",tpsLumTerre:"",particularite:"Planéte grillée"},
+	angelique:{distSol:"108",diam:"12 100 km",rotation:"244 jours",revolution:"225 jours",sat:"0",distTerre:"108 millions km",diamAppMaxi:"58\"",magnMax:"-4,4",tpsLumTerre:"",particularite:"Rotation en sens inverse"},
+	johan:{distSol:"149",diam:"12 750 km",rotation:"24h",revolution:"365 jours",sat:"1",distTerre:"",diamAppMaxi:"",magnMax:"",tpsLumTerre:"-",particularite:"Contiens de la vie"},
+	karine:{distSol:"",diam:"1 474 km",rotation:"29,5 jours",revolution:"29,5 jours",sat:"-",distTerre:"300 000 km",diamAppMaxi:"31\'36\"",magnMax:"-12",tpsLumTerre:"1 sec",particularite:"Sans atmosphere"},
+	mathilde:{distSol:"228",diam:"6 785 km",rotation:"24h 37min",revolution:"687 jours",sat:"2",distTerre:"",diamAppMaxi:"25\"",magnMax:"-2,8",tpsLumTerre:"4 minutes",particularite:"Plus de champs magnetique"},
+	marion:{distSol:"777",diam:"139 820 km",rotation:"9h 53min",revolution:"1ans 11mois",sat:"30",distTerre:"",diamAppMaxi:"50\"",magnMax:"-3",tpsLumTerre:"35 minutes",particularite:"Grande tâche rouge"},
+	yulia:{distSol:"1 428",diam:"1116 460 km",rotation:"10h 25min",revolution:"29ans 6mois",sat:"40",distTerre:"",diamAppMaxi:"20\"",magnMax:"-0,5",tpsLumTerre:"71 minutes",particularite:"Anneaux"},
+};
+var detailPaysage = {
+	canada:{Capitale:"Ottawa",Superficie:"9,98 millions km²",Population:"37,6 millions",nbrReg:"10"},
+	france:{Capitale:"Paris",Superficie:"643 801 km²",Population:"67 millions",nbrReg:"13"},
+	italie:{Capitale:"Rome",Superficie:"301 338 km²",Population:"60,36 millions",nbrReg:"20"},
+	japon:{Capitale:"Tokyo",Superficie:"377 915 km²",Population:"126,5 millions",nbrReg:"8"},
+	martinique:{Capitale:"Paris / Fort-de-france",Superficie:"1 128 km²",Population:"376 480",nbrReg:"1"},
 };
 
 function afficheMiniature(type, sujet){
@@ -52,9 +73,9 @@ function afficheInfo(type, sujet){
 	if (type == "Astro"){
 		var contains = ('<h2>' + newSujet +'</h2><ol><li id="colorTitreTextInfo">Distance du soleil : &nbsp </li><li id="colorTextInfo"> ' + detailAstronomie[sujet].distSol + '</li><br><li id="colorTitreTextInfo">Diamètre : &nbsp </li><li id="colorTextInfo"> ' + detailAstronomie[sujet].diam + '</li><br><li id="colorTitreTextInfo">Tps de révolution : &nbsp </li><li id="colorTextInfo"> ' + detailAstronomie[sujet].revolution + '</li><br><li id="colorTitreTextInfo">Tps de rotation : &nbsp </li><li id="colorTextInfo"> ' + detailAstronomie[sujet].rotation + '</li><br><li id="colorTitreTextInfo">Nbr de satellite : &nbsp </li><li id="colorTextInfo"> ' + detailAstronomie[sujet].sat + '</li><br><br><li id="colorTitreTextInfo">Particularité : &nbsp </li><li id="colorTextInfo"> ' + detailAstronomie[sujet].particularite + '</li><br></br><li id="colorTitreTextInfo">Distance de la terre : &nbsp </li><li id="colorTextInfo"> ' + detailAstronomie[sujet].distTerre + '</li><br><li id="colorTitreTextInfo">Diamètre apparent max : &nbsp </li><li id="colorTextInfo"> ' + detailAstronomie[sujet].diamAppMaxi + '</li><br><li id="colorTitreTextInfo">Magnitude maxi : &nbsp </li><li id="colorTextInfo"> ' + detailAstronomie[sujet].magnMax + '</li><br><li id="colorTitreTextInfo">Dist. lum. de la terre : &nbsp </li><li id="colorTextInfo"> ' + detailAstronomie[sujet].tpsLumTerre + '</li></ol>');
 	} if (type == "Paysage"){
-		var contains = ('<h2>' + newSujet +'</h2><ol><li>Lieu : </li><br><li>Date : </li></ol>');
+		var contains = ('<h2>' + newSujet +'</h2><ol><li id="colorTitreTextInfo">Capitale : &nbsp </li><li id="colorTextInfo"> ' + detailPaysage[sujet].Capitale + '</li><br><li id="colorTitreTextInfo">Superficie : &nbsp </li><li id="colorTextInfo"> ' + detailPaysage[sujet].Superficie + '</li><br><li id="colorTitreTextInfo">Population : &nbsp </li><li id="colorTextInfo"> ' + detailPaysage[sujet].Population + '</li><br><li id="colorTitreTextInfo">Nombre de région : &nbsp </li><li id="colorTextInfo"> ' + detailPaysage[sujet].nbrReg + '</li><br></ol> <br><img id="home" src="../images/Miniatures/' + type + '/' + sujet + ' (0).jpg" width="30%" text-align="center" title="Carte"><br></a></img>&emsp;');
 	} if (type == "Portrait"){
-		var contains = ('<h2>' + newSujet +'</h2><ol><li>Lieux : </li><br><li>Date : </li></ol>');
+		var contains = ('<h2>' + newSujet +'</h2><ol><li id="colorTitreTextInfo">Lieux :&nbsp  </li><br><li>Date : </li></ol>');
 	}
 	document.getElementById('aside1').innerHTML += contains;
 }
@@ -65,7 +86,7 @@ function menuHorizontal(type, sujet){
 	} if (type == "Paysage"){
 		var contains = ('<a href="../index.html" id="titre"><h2>Paysages</h2></a><section><ul><li class="titremenu1"><a href="viewPicture.html?Paysage&canada" target="">Canada</a></li><li class="titremenu2"><a href="viewPicture.html?Paysage&france" target="">France</a></li><li class="titremenu3"><a href="viewPicture.html?Paysage&italie" target="">Italie</a></li><li class="titremenu4"><a href="viewPicture.html?Paysage&japon" target="">Japon</a></li><li class="titremenu5"><a href="viewPicture.html?Paysage&martinique" target="">Martinique</a></li></ul></section>');
 	} if (type == "Portrait"){
-		var contains = ('<a href="../index.html" id="titre"><h2>Portrait</h2></a><section><ul><li class="titremenu1"><a href="viewPicture.html?Portrait&anais" target="">Anaïs</a></li><li class="titremenu2"><a href="viewPicture.html?Portrait&mathilde" target="">Mathilde</a></li> <li class="titremenu3"><a href="viewPicture.html?Portrait&johan" target="">Johan</a></li><li class="titremenu4"><a href="viewPicture.html?Portrait&marion " target="">Marion </a></li><li class="titremenu5"><a href="viewPicture.html?Portrait&yulia " target="">Yulia </a></li><li class="titremenu6"><a href="viewPicture.html?Portrait&karine " target="">Karine </a></li><li class="titremenu7"><a href="viewPicture.html?Portrait&angelique " target="">Angelique </a></li><li class="titremenu8"><a href="viewPicture.html?Portrait&anais2 " target="">Anais2 </a></li></ul></section>');
+		var contains = ('<a href="../index.html" id="titre"><h2>Portrait</h2></a><section><ul><li class="titremenu1"><a href="viewPicture.html?Portrait&anais" target="">Anaïs</a></li><li class="titremenu2"><a href="viewPicture.html?Portrait&anais2 " target="">Anais2 </a></li><li class="titremenu3"><a href="viewPicture.html?Portrait&angelique " target="">Angelique </a></li><li class="titremenu4"><a href="viewPicture.html?Portrait&johan" target="">Johan</a></li><li class="titremenu5"><a href="viewPicture.html?Portrait&karine " target="">Karine </a></li><li class="titremenu6"><a href="viewPicture.html?Portrait&mathilde" target="">Mathilde</a></li><li class="titremenu7"><a href="viewPicture.html?Portrait&marion " target="">Marion </a></li><li class="titremenu8"><a href="viewPicture.html?Portrait&yulia " target="">Yulia </a></li></ul></section>');
 	} if (type == "Animaux"){
 		var contains = ('<a href="../index.html" id="titre"><h2>Animaux</h2></a><section><ul><li class="titremenu1"><a href="viewPicture.html?Portrait&anais" target="">Anaïs</a></li><li class="titremenu2"><a href="viewPicture.html?Portrait&mathilde" target="">Mathilde</a></li><li class="titremenu3"><a href="viewPicture.html?Portrait&johan" target="">Johan</a></li></ul></section>');
 	}
